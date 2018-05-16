e6772035fcc19098fa9ca8323798e036260a0e00
bbqdzt <bbqdzt@gmail.com>
2018-05-16 09:29:30 +0800

    datalist
    datalist change 180516

---
+++ MyWeb276/datalist276.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage276.master" AutoEventWireup="true" CodeFile="datalist276.aspx.cs" Inherits="datalist276" %>
+
+<%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+    <style type="text/css">
+        .auto-style1 {
+            height: 20px;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <asp:DataList ID="DataList1" runat="server" DataKeyField="ProductID" RepeatDirection="Horizontal" ShowHeader="False" CellPadding="4" ForeColor="#333333" RepeatColumns="4" OnItemDataBound="DataList1_ItemDataBound" OnItemCommand="DataList1_ItemCommand">
+        <AlternatingItemStyle BackColor="White" />
+        <FooterStyle BackColor="White" Font-Bold="True" ForeColor="black" />
+        <FooterTemplate>
+            <table style="width: 100%;">
+                <tr>
+                    <td class="auto-style1">共
+                        <asp:Label ID="pageCount" runat="server" Text="Label"></asp:Label>
+                        页
+                    </td>
+                    <td class="auto-style1">当前第<asp:Label ID="currentIndex" runat="server"></asp:Label>
+                        页</td>
+                    <td class="auto-style1">
+                        <asp:LinkButton ID="LinkButton1" runat="server" CommandName="ind">首页</asp:LinkButton>
+                    </td>
+                    <td class="auto-style1">
+                        <asp:LinkButton ID="LinkButton2" runat="server" CommandName="pre">上一页</asp:LinkButton>
+                    </td>
+                    <td class="auto-style1">
+                        <asp:LinkButton ID="LinkButton3" runat="server" CommandName="nex">下一页</asp:LinkButton>
+                    </td>
+                    <td class="auto-style1">
+                        <asp:LinkButton ID="LinkButton4" runat="server" CommandName="las">尾页</asp:LinkButton>
+                    </td>
+                    <td class="auto-style1">跳到<asp:TextBox ID="tex" runat="server" Width="32px" ></asp:TextBox>页
+            <asp:LinkButton ID="LinkButton5" runat="server" CommandName="go">Go</asp:LinkButton>
+                    </td>
+                    <%-- <td class="auto-style1">&nbsp;</td>--%>
+                </tr>
+
+
+            </table>
+        </FooterTemplate>
+        <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+        <ItemStyle BackColor="#FFFBD6" ForeColor="#333333" />
+        <ItemTemplate>
+
+            <asp:Image ID="Image1" runat="server" ImageUrl="~/image/food.jpg" />
+            <br />
+            ProductID:
+            <%--<asp:Label ID="ProductIDLabel" runat="server" Text='<%# Eval("ProductID") %>' />--%>
+            <asp:HyperLink ID="HyperLink1" runat="server" Text='<%# Eval("ProductID") %>'></asp:HyperLink>
+            <br />
+            Unit:
+            <asp:Label ID="UnitLabel" runat="server" Text='<%# Eval("Unit") %>' />
+            <br />
+            Storage:
+            <asp:Label ID="StorageLabel" runat="server" Text='<%# Eval("Storage") %>' />
+            <br />
+            Buy:
+            <asp:HyperLink ID="HyperLink2" runat="server" ImageUrl="~/image/food.jpg" ImageHeight="20px" ImageWidth="100px"></asp:HyperLink>
+        </ItemTemplate>
+
+        <SelectedItemStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+
+    </asp:DataList>
+
+
+
+</asp:Content>
+
---
+++ MyWeb276/datalist276.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+public partial class datalist276 : System.Web.UI.Page
+{
+    public static PagedDataSource ps = new PagedDataSource();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {   
+        if (!IsPostBack) {
+            databind(0); 
+        }
+
+    }
+
+    private void databind( int currentIndex)
+    {
+        string strconn = System.Configuration.ConfigurationManager.ConnectionStrings["shopdataConnectionString"].ToString();
+        SqlConnection conn = new SqlConnection(strconn);
+
+        string cmdstr = "select * from Product";
+        try
+        {
+            SqlDataAdapter ada = new SqlDataAdapter(cmdstr, conn);
+            DataSet ds = new DataSet();
+            
+            ada.Fill(ds, "shopdataConnectionString");
+            ps.DataSource = ds.Tables["shopdataConnectionString"].DefaultView;
+            ps.AllowPaging = true;
+            ps.PageSize = 8;
+            ps.CurrentPageIndex = currentIndex;
+
+
+            DataList1.DataSource = ps;
+            DataList1.DataKeyField = "ProductID";
+            DataList1.DataBind();
+        }
+        catch
+        {
+            Response.Write("no page");
+        }
+    }
+
+
+
+    protected void DataList1_ItemDataBound(object sender, DataListItemEventArgs e)
+    {
+        Response.Write("databound");
+        if (e.Item.ItemType == ListItemType.Footer)
+        {
+            Response.Write("databinding");
+            Label pageCount = e.Item.FindControl("pageCount") as Label;
+            Label currentIndex =  e.Item.FindControl("currentIndex") as Label;
+            LinkButton ind = e.Item.FindControl("LinkButton1") as LinkButton;
+            LinkButton pre = e.Item.FindControl("LinkButton2") as LinkButton;
+            LinkButton nex = e.Item.FindControl("LinkButton3") as LinkButton;
+            LinkButton las = e.Item.FindControl("LinkButton4") as LinkButton;
+            //LinkButton go = e.Item.FindControl("LinkButton5") as ;
+
+            pageCount.Text = ps.PageCount.ToString();
+            currentIndex.Text = (ps.CurrentPageIndex +1).ToString();
+
+            if (ps.IsFirstPage)
+            {
+                ind.Enabled = false;
+                pre.Enabled = false;
+            }
+            if (ps.IsLastPage)
+            {
+                nex.Enabled = false;
+                las.Enabled = false;
+            }
+        }
+    }
+
+    protected void DataList1_ItemCommand(object source, DataListCommandEventArgs e)
+    {
+        switch (e.CommandName)
+        {
+            case "ind":
+                ps.CurrentPageIndex =0;
+                databind(0);
+                Response.Write("ind");
+                break;
+            case "pre":
+                ps.CurrentPageIndex -= 1;
+                databind(ps.CurrentPageIndex);
+                break;
+            case "nex":
+                ps.CurrentPageIndex += 1;
+                databind(ps.CurrentPageIndex);
+                break;
+            case "las":
+                ps.CurrentPageIndex = ps.PageCount-1;
+                databind(ps.CurrentPageIndex);
+                break;
+            case "go":
+                try { 
+                String texS = "";
+                if(e.Item.ItemType == ListItemType.Footer)
+                {
+                    TextBox tex = e.Item.FindControl("tex") as TextBox;
+                    texS = tex.Text;
+                }
+                int texN = 0;
+                texN = Convert.ToInt32(texS)-1;
+                databind(texN);
+                
+                }
+                catch
+                {
+                    Response.Write("nopage");
+                }
+                break;
+        }
+    }
+}
